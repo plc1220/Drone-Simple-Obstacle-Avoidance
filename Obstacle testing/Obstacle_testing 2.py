1888d8ea385e95b5fc0d76fcd9e177df59a5437d
plc1220 <51946394+plc1220@users.noreply.github.com>
2019-10-14 18:03:28 +0800

    First Commit
    Obstacle_testing.py can use to play around with SITL without the RPLidar sensor, sensor data is substitute by random number.
    Try.py is testing code with the sensor only while Obstacle_testing 2 is the one working with sensor and drone.
    Just change the connection string to respective port to connect accordingly.

---
+++ Obstacle testing/Obstacle_testing 2.py
@@ -0,0 +1,96 @@
+from dronekit import connect, VehicleMode
+import sys
+from rplidar import RPLidar
+import time
+
+
+# Connect to Mission Planner SITL
+vehicle = connect('127.0.0.1:14551',wait_ready=True)
+
+# Defining distance sensor mavlink message generation function
+def sensor_data(d,o):
+    msg=vehicle.message_factory.distance_sensor_encode(
+        0,
+        5,
+        1000,
+        d,
+        0,
+        1,
+        o,
+        0,
+        )
+    vehicle.send_mavlink(msg)
+    print (msg)
+
+# Defining distance sensor data classification
+def orientation(a):
+    global o
+    if (0<= a and a<= 45):
+          o = 0
+
+    elif (46<= a and a<=90):
+         o = 1
+
+    elif (91<= a and a<= 135):
+          o = 2
+            
+    elif (136<= a and a<=180):
+            o = 3
+    
+    elif (181<= a and a<=225):
+            o = 4
+
+    elif (226<= a and a<= 270):
+         o = 5
+
+    elif (271<= a and a<=315):
+        o = 6
+
+    elif (316<= a and a<=359):
+         o = 7
+
+    return o
+
+# Connect to lidar
+PORT_NAME = 'COM3'
+lidar = RPLidar(PORT_NAME)
+health = lidar.get_health()
+print(health)
+
+# Make continuous
+while vehicle.mode.name !=('LAND'or'RTL'):
+    print("Mode: %s" % vehicle.mode.name)
+# Simulate Obstacle avoidance
+    i=0
+    for measurement in lidar.iter_measurments():
+        a = measurement[2]
+        dRaw = (measurement[3])/10
+        d = round(dRaw)
+        print(d)
+        print(a)
+
+        o = int(orientation(a))
+        print(o)
+        if d>=0:
+            i+=1
+            print('ok'+str(i))
+            sensor_data(int(d),o)
+        else:
+# Override RC, Channel 5(Flight Mode)
+##print('set Flight Mode to RTL')
+##t_end = time.time() + 60 * 5
+##while time.time() < t_end:
+##    vehicle.channels.overrides['5'] = 1685
+
+# Swtich Flight mode to 'Brake'
+            vehicle.mode = VehicleMode("BRAKE")
+            print("Obstacle detected!, Flight mode forced: %s" % vehicle.mode.name)
+            time.sleep(10)
+        if vehicle.mode.name == 'BRAKE':
+            print("No pilot response, A/C initiates RTL")
+            print("Mode: %s" % vehicle.mode.name)
+            vehicle.mode = VehicleMode('RTL')
+            time.sleep(10)
+        if vehicle.mode.name =='LAND':
+            break
+    break
